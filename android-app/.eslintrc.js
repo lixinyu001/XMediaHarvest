ebb241cf9ab73350ac30a3180662cad57b326326
lixinyu001 <lixinyu31415001@163.com>
2026-02-17 18:03:46 +0800

    123

--- android-app/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'prettier/prettier': 0,
-  },
-};
